6287f722f8cc0a2a7b76e1a11ab0acf15c3c2769
ktorres24 <31192254+ktorres24@users.noreply.github.com>
2017-11-01 20:56:11 -0400

    Fixed
    Fixed

---
+++ src/PWM_helper.h
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include "hw_ints.h"
+#include "hw_memmap.h"
+#include "pin_map.h"
+#include "hw_timer.h"
+#include "hw_gpio.h"
+#include "sysctl.h"
+#include "timer.h"
+#include "gpio.h"
+#include "hw_pwm.h"
+#include "pwm.h"
+
+#define time 56666
+
+
+void PWM_setup(void);
+void functions(void);
