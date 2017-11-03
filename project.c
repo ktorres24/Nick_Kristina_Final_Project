4b4b51f8c00969f64afd8b128109b8afbdb60a0f
ktorres24 <31192254+ktorres24@users.noreply.github.com>
2017-11-02 22:09:35 -0400

    Buttons
    Buttons are working

--- project.c
+++ project.c
@@ -21,6 +21,7 @@ int GPIO_FWD;
 int GPIO_RIGHT;
 int GPIO_BACK;
 
+
 int main()
 {
 	Gpio_setup();
@@ -30,38 +31,48 @@ int main()
 	//if another button is also pressed (buttons won't be pressed "simultaneously", right?)
 	
 	//psuedo code for now
-	while x == 1
+	while (1)
 	{
+
+		GPIO_LEFT = GPIOPinRead(GPIO_PORTF_BASE, GPIO_PIN_4);
+		GPIO_FWD = GPIOPinRead(GPIO_PORTD_BASE, GPIO_PIN_6);
+		GPIO_RIGHT = GPIOPinRead(GPIO_PORTF_BASE, GPIO_PIN_0);
+		GPIO_BACK = GPIOPinRead(GPIO_PORTD_BASE, GPIO_PIN_7);
+		
 		//FORWARD
-		if(GPIO_FWD == 1)
-		{ 
-		// FORWARD RIGHT TURN
-		if(GPIO_FWD == 1 & GPIO_RIGHT ==1)
+		if((GPIO_FWD & GPIO_PIN_6) == 0)
 		{
+		// FORWARD RIGHT TURN
+		if((GPIO_RIGHT & GPIO_PIN_0)== 0)
+		{
+			GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_1, GPIO_PIN_1);
 			//send pwm signal, (Left side speed > Right side speed)
 		}
-		//FORWARD RIGHT TURN
-		else if ( (GPIO_FWD == 1 & GPIO_LEFT ==1)
+		//FORWARD LEFT TURN
+		else if ( (GPIO_LEFT & GPIO_PIN_4)==0)
 		{
 			//send pwm signal, (Right side speed > Left side speed)
 		}
 		//FORWARD
 		else 
 		{
+		
+        PWMPulseWidthSet(PWM1_BASE, PWM_OUT_2,320);
+   
 			//send pwm signal
 		}
 		}
 		
 		//BACKWARDS
-		if(GPIO_BACK == 1)
+		if((GPIO_BACK & GPIO_PIN_7) == 0)
 		{		
 		//BACK & RIGHT
-		if(GPIO_BACK == 1 & GPIO_RIGHT ==1)
+		if((GPIO_RIGHT & GPIO_PIN_0)==0)
 		{
 			//send pwm signal, (Negative Left side speed > Negative Right side speed)
 		}
 		//BACK & LEFT
-		else if(GPIO_BACK == 1 & GPIO_LEFT ==1)
+		else if((GPIO_LEFT & GPIO_PIN_4) ==0)
 		{
 			//send pwm signal, (Negative Right side speed > Negative Left side speed)
 		}
@@ -73,13 +84,13 @@ int main()
 		}
 		
 		//LEFT (0 turn)
-		if(GPIO_LEFT == 1)
+		if((GPIO_LEFT & GPIO_PIN_4) == 0)
 		{
 			//send pwm signal
 		}
 		
 		//RIGHT (0 turn)
-		if(GPIO_RIGHT == 1)
+		if((GPIO_RIGHT & GPIO_PIN_0)== 0)
 		{
 			//send pwm signal
 		}
