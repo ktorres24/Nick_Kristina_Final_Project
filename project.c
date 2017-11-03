7ea953caf66df8f97300e6f89b39aa8395361d2c
ktorres24 <31192254+ktorres24@users.noreply.github.com>
2017-11-03 17:31:35 -0400

    Buttons again
    Lights stay on when button pushed off when not

--- project.c
+++ project.c
@@ -24,8 +24,8 @@ int GPIO_BACK;
 
 int main()
 {
-	Gpio_setup();
-	PWM_setup();
+   Gpio_setup();
+	//PWM_setup();
 	
 	//need to set up some sort of delay/checking period after a button is pressed so that our code can check and see 
 	//if another button is also pressed (buttons won't be pressed "simultaneously", right?)
@@ -45,26 +45,28 @@ int main()
 		// FORWARD RIGHT TURN
 		if((GPIO_RIGHT & GPIO_PIN_0)== 0)
 		{
-			GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_1, GPIO_PIN_1);
+			GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1, GPIO_PIN_1);
 			//send pwm signal, (Left side speed > Right side speed)
+		
+			GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1, 0);
 		}
 		//FORWARD LEFT TURN
-		else if ( (GPIO_LEFT & GPIO_PIN_4)==0)
+		else if ( (GPIO_LEFT & GPIO_PIN_7)==0)
 		{
 			//send pwm signal, (Right side speed > Left side speed)
 		}
 		//FORWARD
 		else 
 		{
-		
-        PWMPulseWidthSet(PWM1_BASE, PWM_OUT_2,320);
-   
+			GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, GPIO_PIN_2);
 			//send pwm signal
+			GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, 0);
 		}
 		}
+		else{}
 		
 		//BACKWARDS
-		if((GPIO_BACK & GPIO_PIN_7) == 0)
+		if((GPIO_BACK & GPIO_PIN_6) == 0)
 		{		
 		//BACK & RIGHT
 		if((GPIO_RIGHT & GPIO_PIN_0)==0)
@@ -79,21 +81,35 @@ int main()
 		//BACKWARDS
 		else
 		{
+			GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1, GPIO_PIN_1);
+			//send pwm signal, (Left side speed > Right side speed)
+				GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, GPIO_PIN_2);
+			//send pwm signal
+			GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_2, 0);
+		
+			GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1, 0);
+			
+			
 			//send pwm signal
 		}
 		}
+		else{}
 		
 		//LEFT (0 turn)
 		if((GPIO_LEFT & GPIO_PIN_4) == 0)
 		{
+			GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_3, GPIO_PIN_3);
 			//send pwm signal
+			GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_3, 0);
 		}
+
 		
 		//RIGHT (0 turn)
 		if((GPIO_RIGHT & GPIO_PIN_0)== 0)
 		{
 			//send pwm signal
 		}
+	
 	
 		
 	
